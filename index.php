fc783cbd5efc8cb7d24a28d0634d3062668d778d
Seyni Baldé <senseybalde@gmail.com>
2025-03-08 15:20:15 +0000

    style(vehicles): Ajout de la classe btn-primary au bouton de détails
    - Amélioration du style du bouton de détails des véhicules
    - Application de la classe Bootstrap btn-primary pour un design cohérent
    - Mise à jour visuelle du bouton de navigation vers la page de détails

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ ob_start();
                             <span><i class="fas fa-gas-pump"></i> <?= htmlspecialchars($vehicle['carburant']) ?></span>
                             <span><i class="fas fa-cog"></i> <?= htmlspecialchars($vehicle['transmission']) ?></span>
                         </div>
-                        <a href="<?= getBaseUrl() ?>vehicule/detail?id_vehicule=<?= $vehicle['id'] ?>" class="btn">
+                        <a href="<?= getBaseUrl() ?>vehicule/detail?id_vehicule=<?= $vehicle['id'] ?>" class="btn btn-primary">
                             <i class="fas fa-eye"></i>
                             Voir détails
                         </a>
