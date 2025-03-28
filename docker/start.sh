676f47bc90c114e87025813f8f05d6f1a5dc1a69
Seyni Baldé <senseybalde@gmail.com>
2025-03-28 10:45:11 +0000

    supprimer les fichiers de configuration obsolètes, y compris .env.example, composer.json, Dockerfile, et d'autres fichiers liés à la configuration de l'environnement, afin de nettoyer le projet et de réduire la complexité.

--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remplace le port dans la configuration Apache
-sed -i "s/\${PORT}/$PORT/g" /etc/apache2/sites-available/000-default.conf
-
-# Démarre Apache en premier plan
-apache2-foreground 
